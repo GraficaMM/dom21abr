e70a8d5649597a918a3ec3406cdc2ae4e0ae3f04
GraficaMM <grafica.multimedia.2023@gmail.com>
2024-04-24 03:01:40 +0200

    correcciones en la app

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,12 @@ import PageHeader from './components/PageHeader.vue'
 import axiosClient from './utils/axios'
 
 const pokemonlist = ref([])
+const pokemonselect = ref({} as any)
 
 const fetchPokemon = async () => {
   try {
     const { data } = await axiosClient.get('/pokemon')
-    pokemonlist.value = data
+    pokemonlist.value = data.results
   } catch (error) {
     console.log(error)
   }
@@ -16,11 +17,29 @@ const fetchPokemon = async () => {
 onMounted(() => {
   fetchPokemon()
 })
+
+const detallePokemon = async (nombre: string) => {
+  try {
+    const { data } = await axiosClient.get(`/pokemon/${nombre}`)
+    pokemonselect.value = data
+  } catch (error) {
+    console.log(error)
+  }
+}
 </script>
 
 <template>
   <PageHeader />
-  <div v-for="pokemon in pokemonlist" v-bind:key="pokemon">
-    {{ pokemon[name] }}
+  <div class="flex">
+    <div class="flex flex-col">
+      <div v-for="pokemon in pokemonlist" v-bind:key="pokemon">
+        <h2 @click="detallePokemon(pokemon['name'])">
+          {{ pokemon['name'] }}
+        </h2>
+      </div>
+    </div>
+    <div v-if="Object.keys(pokemonselect).length > 0">
+      <img :src="pokemonselect.sprites.front_default" />
+    </div>
   </div>
 </template>
